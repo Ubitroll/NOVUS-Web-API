9672650a39b1f75ea6630f1187b0c87a0de7011c
Ubitroll <stephencartner@hotmail.co.uk>
2023-03-23 10:46:38 +0000

    WebAPI Restful completed

---
+++ NovusWebAPI/WebAPI/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPI.Controllers
+{
+    // ../api/Home
+    public class HomeController : ApiController
+    {
+        private string[] S = { "apple", "banana", "orange" };
+
+        public string[] Get()
+        { 
+            return S; 
+        }
+
+        // ../api/Home/{id}
+        public string Get(int id)
+        {
+            return S[id];
+        }
+    }
+}
